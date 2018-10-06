266c5eb72b9606599b13a70a89b777701573a666
Pierre-Luc Bruyere <proteama@yahoo.com>
2018-10-06 11:39:51 -0400

    Randomized dices

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+  @IBOutlet weak var diceImageView1: UIImageView!
+  @IBOutlet weak var diceImageView2: UIImageView!
 
-  override func viewDidLoad() {
+  lazy private var diceImages : [UIImage?] = []
+
+  override func viewDidLoad()
+  {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+
+    for index in 1...6
+    {
+      diceImages.append(UIImage(named: "dice" + index.description))
+    }
+
+    pickDices()
   }
 
+  @IBAction func rollDice(_ sender: UIButton)
+  {
+    pickDices()
+  }
 
+  private func pickDices()
+  {
+    let dice1 = Int.random(in: 0..<diceImages.count)
+    diceImageView1.image = diceImages[dice1]
+
+    let dice2 = Int.random(in: 0..<diceImages.count)
+    diceImageView2.image = diceImages[dice2]
+  }
 }
 
